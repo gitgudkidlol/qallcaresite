f9a44232214d5a2c4bb13d167ce36a45fec379f9
gitgudkidlol <80183185+gitgudkidlol@users.noreply.github.com>
2021-03-07 08:12:22 -0500

    update

--- main/urls.py
+++ main/urls.py
@@ -6,9 +6,8 @@ urlpatterns =[
 		path("corona/", views.corona, name ="corona"),
 		path("services/", views.services, name ="services"),
 		path("contact/", views.contact, name ="contact"),
-		path("clinics/", views.displayClinics, name ="displayClinics"),
-		path("clinics/<int:id>",views.display, name="display"),
-		# path("upload/",views.upload, name="upload"),
+		path("clients/", views.displayClients, name ="displayClients"),
+		path("employees/", views.displayEmployees, name ="display"),
 		path("login/",views.login, name="login"),
 
 ]
