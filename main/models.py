93bc7126b6bfa0807b96c481d7ee4f8f7e414dd7
gitgudkidlol <80183185+gitgudkidlol@users.noreply.github.com>
2021-03-06 23:28:34 -0500

    Update models.py

--- main/models.py
+++ main/models.py
@@ -12,7 +12,7 @@ class Patient(models.Model):
 	firstname = models.CharField(max_length=200)
 	lastname= models.CharField(max_length=200)
 	diagnosis=models.BooleanField()
-	id=models.int()
+	
 
 	def __str__(self):
 		return (self.firstname + self.lastname)
